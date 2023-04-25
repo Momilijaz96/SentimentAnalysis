da25f62618783a4ea9e59ea0480f760e41c13290
Momilijaz96 <momilijaz@gmail.com>
2023-04-25 17:54:52 +0500

    fixing the react spa comm with BE

--- frontend/spa/src/App.js
+++ frontend/spa/src/App.js
@@ -2,7 +2,24 @@ import React, { useState } from "react";
 
 const App = () => {
   const [text, setText] = useState("");
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
 
+
+
+    const fetchData = async () => {
+      try {
+      const response = await fetch("http://127.0.0.1:8000/predict");
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+      setError("");
+    } catch (error) {
+      setError(error.message);
+    };
+    fetchData();
+
+  };
   return (
     <div>
       <h1>Tweet Sentiment Analysis</h1>
@@ -17,7 +34,11 @@ const App = () => {
         onChange={(e) => setText(e.target.value)}
         onFocus={() => setText("Enter tweet here...")}
       />
-      <button onClick={() => alert(text)}>Analyze Sentiment</button>
+      <button onClick={(fetchData)}>Analyze Sentiment</button>
+      <textarea
+        value={error ? error : JSON.stringify(data)}
+        onChange={(e) => setData(e.target.value)}
+      />
     </div>
   );
 };
